9126226f519ad348e0063dc1fc527c5a5069e2bb
Brave Hager <bravehager7@gmail.com>
2019-06-30 16:20:54 -0400

    Add evaluate example

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -15,9 +15,15 @@ program
     .command("run <file>")
     .action(async (file, cmd) => {
         const parser: Parser = new Parser();
-        const contents: string = fs.readFileSync(path.join(".", file), { encoding: "utf-8" });
-        const routine: Routine = parser.parse({ contents });
-        await routine.run(await puppeteer.launch({ headless: false }));
+        
+        try {
+            const contents: string = fs.readFileSync(path.join(".", file), { encoding: "utf-8" });
+            const routine: Routine = parser.parse({ contents });
+            await routine.run(await puppeteer.launch({ headless: false }));
+        } catch (error) {
+            console.log(`No such file ${path.join(".", file)}`);
+            process.exit();
+        }
     });
 
 program.parse(process.argv);
